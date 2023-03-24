9a902ac9598f70777ce0ca5ac8b6cda69ecdb5a9
Kamel Maredia <kamilmaredia23@yahoo.com>
2023-03-24 14:23:49 -0500

    Added to unit test

--- Assignment2/test_unit.py
+++ Assignment2/test_unit.py
@@ -6,3 +6,9 @@ class Test(unittest.TestCase):
         def test_create_profile(self):
             response = requests.get('http://127.0.0.1:5000/client_profile')
             self.assertEqual(response.status_code, 200)
+        
+        def test_full_name_length(self):
+            response = requests.post('http://127.0.0.1:5000/client_profile',
+                                 data={'fullname': '', 'address': 'abcd', 'address2': '1234',
+                                       'city': 'Houston', 'state': 'TX', 'zip code': '1111111'})
+            self.assertTrue('Full name must be between 1 and 50 characters.' in response.text)
